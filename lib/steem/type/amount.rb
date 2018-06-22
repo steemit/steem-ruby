abb11b11914552d48dab926ab2f7aca63d7d2060
Anthony Martin <github@martin-studio.com>
2018-06-22 08:39:39 -0700

    added option to use condenser_api for broadcast, defaults appbase

--- lib/steem/type/amount.rb
+++ lib/steem/type/amount.rb
@@ -3,8 +3,10 @@ module Steem
     
     # See: https://github.com/xeroc/piston-lib/blob/34a7525cee119ec9b24a99577ede2d54466fca0e/steembase/operations.py
     class Amount < BaseType
-      def self.to_nia(amount)
-        new(amount).to_nia
+      attr_reader :amount, :precision, :nai, :asset
+      
+      def self.to_h(amount)
+        new(amount).to_h
       end
       
       def self.to_s(amount)
@@ -16,15 +18,33 @@ module Steem
         
         case value
         when Array
-          a, p, t = value
-          @asset = case t
+          @amount, @precision, @nai = value
+          @asset = case @nai
           when '@@000000013' then 'SBD'
           when '@@000000021' then 'STEEM'
           when '@@000000037' then 'VESTS'
-          else; raise TypeError, "Asset #{@asset} unknown."
+          else; raise TypeError, "Asset #{@nai} unknown."
           end
-          @precision = p
-          @amount = "%.#{p}f" % (a.to_f / 10 ** p)
+          
+          @amount = "%.#{@precision}f" % (@amount.to_f / 10 ** @precision)
+        when Hash
+          @amount, @precision, @nai = value.map do |k, v|
+            v if %i(amount precision nai).include? k.to_sym
+          end.compact
+          
+          @asset = case @nai
+          when '@@000000013' then 'SBD'
+          when '@@000000021' then 'STEEM'
+          when '@@000000037' then 'VESTS'
+          else; raise TypeError, "Asset #{@nai} unknown."
+          end
+          
+          @amount = "%.#{@precision}f" % (@amount.to_f / 10 ** @precision)
+        when Amount
+          @precision = value.precision
+          @nai = value.nai
+          @asset = value.asset
+          @amount = value.amount
         else
           @amount, @asset = value.strip.split(' ') rescue ['', '']
           @precision = case @asset
@@ -45,11 +65,31 @@ module Steem
         asset
       end
       
-      def to_nia
+      def to_a
         case @asset
         when 'STEEM' then [(@amount.to_f * 1000).to_i.to_s, 3, '@@000000021']
         when 'VESTS' then [(@amount.to_f * 1000000).to_i.to_s, 6, '@@000000037']
         when 'SBD' then [(@amount.to_f * 1000).to_i.to_s, 3, '@@000000013']
+        end
+      end
+      
+      def to_h
+        case @asset
+        when 'STEEM' then {
+          amount: (@amount.to_f * 1000).to_i.to_s,
+          precision: 3,
+          nai: '@@000000021'
+        }
+        when 'VESTS' then {
+          amount: (@amount.to_f * 1000000).to_i.to_s,
+          precision: 6,
+          nai: '@@000000037'
+        }
+        when 'SBD' then {
+          amount: (@amount.to_f * 1000).to_i.to_s,
+          precision: 3,
+          nai: '@@000000013'
+        }
         end
       end
       
